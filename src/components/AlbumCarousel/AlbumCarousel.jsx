55634fccae3355e9c9ecd4d5014ff316e4600179
ellis0n <100211236+ellis0n@users.noreply.github.com>
2023-09-21 21:43:03 -0230

    titling

--- src/components/AlbumCarousel/AlbumCarousel.jsx
+++ src/components/AlbumCarousel/AlbumCarousel.jsx
@@ -100,7 +100,7 @@ const AlbumCarousel = () => {
 	return (
 		<div>
 			<div
-				className="album-carousel mt-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4"
+				className="album-carousel mt-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2"
 				onClick={(e) => {
 					setScroll(!active);
 				}}
