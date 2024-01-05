207afa196ee9b3a8655720ea73e1aebcddbc01a3
ellis0n <100211236+ellis0n@users.noreply.github.com>
2024-01-04 21:42:19 -0330

    messing with header, update, intersection

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,6 @@
 const colors = require("tailwindcss/colors");
 const defaultTheme = require("tailwindcss/defaultTheme");
+const plugin = require('tailwindcss/plugin');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -13,5 +14,12 @@ module.exports = {
 			},
 		},
 	},
-	plugins: [require("@tailwindcss/typography")],
+	plugins:
+		[
+			plugin(function ({ addVariant }) {
+				addVariant('scroll', ':merge(.scroll) &');
+				addVariant('is-checked', ':merge(:checked +) &');
+			}), 
+			require("@tailwindcss/typography")
+		]
 };
